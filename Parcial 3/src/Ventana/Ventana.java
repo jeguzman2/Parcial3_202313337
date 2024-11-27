8d3d3641cfeab9f4e03a7d9ea8e9a5dafe615204
jeguzman2 <je.guzman2@uniandes.edu.co>
2024-11-27 08:58:59 -0500

    labels y textfields

--- Parcial 3/src/Ventana/Ventana.java
+++ Parcial 3/src/Ventana/Ventana.java
@@ -6,10 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Controlador.ventanaContralodora;
 public class Ventana extends JFrame {
-    private JTextField txtR;
-    private JTextField txtG;
-    private JTextField txtB;
-    private JPanel panelDibujo;
+    private JTextField textoR;
+    private JTextField textoG;
+    private JTextField textoB;
+    private JPanel panel;
     private ventanaContralodora controlador;
 
     public Ventana() {
@@ -17,6 +17,25 @@ public class Ventana extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(600, 400);
         setLayout(new BorderLayout());
+
+        controlador = new ventanaContralodora();
+        JPanel panelIzquierdo = new JPanel();
+        panelIzquierdo.setLayout(new GridLayout(4, 2, 10, 10));
+        panelIzquierdo.setPreferredSize(new Dimension(200, 0));
+        JLabel labelR = new JLabel("R");
+        JLabel labelG = new JLabel("G");
+        JLabel labelB = new JLabel("B");
+        textoR = new JTextField("0");
+        textoG = new JTextField("0");
+        textoB = new JTextField("0");
+        panelIzquierdo.add(labelR);
+        panelIzquierdo.add(textoR);
+        panelIzquierdo.add(labelG);
+        panelIzquierdo.add(textoG);
+        panelIzquierdo.add(labelB);
+        panelIzquierdo.add(textoB);
+
+
     }
 }
 
