6af10f224a98e50991742b1eb1f14cfd265ccdbe
jeguzman2 <je.guzman2@uniandes.edu.co>
2024-11-27 08:51:21 -0500

    Clase ventana creada

---
+++ Parcial 3/src/Ventana/Ventana.java
@@ -0,0 +1,11 @@
+package Ventana;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Controlador.ventanaContralodora;
+public class Ventana extends JFrame {
+    
+}
