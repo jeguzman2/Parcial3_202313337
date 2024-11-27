66f99fbc0f621cfa0d0abb2e32fd7712849650ac
jeguzman2 <je.guzman2@uniandes.edu.co>
2024-11-27 08:52:28 -0500

    si

--- Parcial 3/src/Ventana/Ventana.java
+++ Parcial 3/src/Ventana/Ventana.java
@@ -1,11 +1,14 @@
 package Ventana;
 
-import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Controlador.ventanaContralodora;
 public class Ventana extends JFrame {
-    
+    private JTextField txtR;
+    private JTextField txtG;
+    private JTextField txtB;
+    private JPanel panelDibujo;
+    private ventanaContralodora controlador;
 }
